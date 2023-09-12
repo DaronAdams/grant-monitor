03677255f85c292ff7775219cac805730d8b7d87
DaronAdams <daron.cole@icloud.com>
2023-09-12 17:29:41 -0500

    Chore: Add material-tailwind to project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@material-tailwind/react';
 
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Button color="blue">Hello World</Button>
   );
 }
 
