92aff9c919a9f829d7b6c5771d614afaa7264bcb
DaronAdams <daron.cole@icloud.com>
2023-09-17 10:11:39 -0500

    Feat: Homepage Sidebar

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,58 @@
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  ShoppingBagIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  PowerIcon,
+} from "@heroicons/react/24/solid";
+ 
+export function HomePageSidebar() {
+  return (
+    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+      <div className="mb-2 p-4">
+        <Typography variant="h5" color="blue-gray">
+          Grant Management
+        </Typography>
+      </div>
+      <List>
+        <ListItem>
+          <ListItemPrefix>
+            <PresentationChartBarIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Dashboard
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <ShoppingBagIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Reports
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <UserCircleIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Profile
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <Cog6ToothIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Settings
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Log Out
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,20 @@
+import { Card, Typography } from "@material-tailwind/react";
+import { HomePageSidebar } from "../components/Sidebar";
+
 const HomePage = () => {
     return ( 
-        <div className="text-3xl">Welcome to the homepage</div>
+        <>
+            <div className="flex flex-row">
+                <HomePageSidebar />
+                <div className="flex flex-col w-full">
+                    <div className="flex flex-row justify-between items-center p-4">
+                        <h1 className="text-2xl font-semibold">Dashboard</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+        
+
      );
 }
  
