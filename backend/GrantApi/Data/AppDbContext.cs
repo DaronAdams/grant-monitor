b5e59ede71dadfe342d567b2a5bf79c7ea5c7021
kenschnarrs <knschnarrs@gmail.com>
2023-09-21 22:19:37 -0500

    set up database connection

---
+++ backend/GrantApi/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using GrantApi.Models;
+
+public class AppDbContext : DbContext
+{
+     protected readonly IConfiguration Configuration;
+
+    public AppDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to postgres with connection string from app settings
+        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+    }
+
+    public DbSet<Grant> Grants { get; set; }
+}
