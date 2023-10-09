ad1699cb692c2335a2d3af2a869f67f3c7948450
DaronAdams <daron.cole@icloud.com>
2023-10-09 12:53:10 -0500

    Feat: Add logic to registration page

--- frontend/src/constants/endpoints.ts
+++ frontend/src/constants/endpoints.ts
@@ -1,2 +1,3 @@
 // All endpoints should be defined here
 export const grantListEndpoint = 'Grant/';
+export const authRegisterEndpoint = 'auth/register/';
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,11 +1,48 @@
+import { useState } from "react";
+import axios from "axios";
 import {
   Card,
   Input,
   Button,
   Typography,
 } from "@material-tailwind/react";
+import { authRegisterEndpoint } from "../constants/endpoints";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    axios
+      .post(authRegisterEndpoint, formData)
+      .then((response) => {
+        console.log("Registration successful", response);
+
+        // Clear the form data after successful registration
+        setFormData({
+          email: '',
+          password: '',
+          confirmPassword: '',
+        })
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+
     return ( 
         <div className="bg-gray-100 flex h-screen items-center justify-center">
             <Card color="transparent" shadow={false}>
@@ -15,13 +52,49 @@ const Register = () => {
       <Typography color="gray" className="mt-1 font-normal">
         Please sign up below.
       </Typography>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+      <form 
+        className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-4 flex flex-col gap-6">
-          <Input size="lg" label="Email" crossOrigin={""} />
-          <Input type="password" size="lg" label="Password" crossOrigin={""} />
-          <Input type="password" size="lg" label="Confirm Password" crossOrigin={""} />
+          <Input 
+            size="lg"
+            label="Email"
+            crossOrigin={""}
+            name="email"
+            type="text"
+            value={formData.email}
+            onChange={handleChange}
+            required 
+          />
+          <Input 
+            type="password"
+            size="lg"
+            label="Password"
+            crossOrigin={""}
+            typeof="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <Input
+            type="password"
+            size="lg"
+            label="Confirm Password"
+            crossOrigin={""}
+            typeof="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <Button className="mt-6" fullWidth>
+        <Button
+          className="mt-6"
+          fullWidth
+          type="submit"
+        >
           Register
         </Button>
         <Typography color="gray" className="mt-4 text-center font-normal">
