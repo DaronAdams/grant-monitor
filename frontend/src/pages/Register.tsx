ec5233f6d933356ffbdbb3b3c15058ecfc5558e1
DaronAdams <daron.cole@icloud.com>
2023-10-09 18:37:59 -0500

    Feat: Connect api to the register and login pages

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -7,7 +7,7 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 import { authRegisterEndpoint } from "../constants/endpoints";
 import { parseErrorsJson } from "../utils/parseJson";
 
@@ -26,6 +26,9 @@ const Register = () => {
     });
   };
 
+  /*
+  * Uses the helper function to parse the message into a user friendly format 
+  */
   const handleErrors = (error: any) => {
     const response = parseErrorsJson(error);
     toast.error(response);
@@ -54,70 +57,68 @@ const Register = () => {
 
     return ( 
         <div className="bg-gray-100 flex h-screen items-center justify-center">
-            <Card color="transparent" shadow={false}>
-      <Typography variant="h4" color="blue-gray">
-        Register
-      </Typography>
-      <Typography color="gray" className="mt-1 font-normal">
-        Please sign up below.
-      </Typography>
-      <form 
-        className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
-        onSubmit={handleSubmit}
-      >
-        <div className="mb-4 flex flex-col gap-6">
-          <Input 
-            size="lg"
-            label="Email"
-            crossOrigin={""}
-            name="email"
-            type="text"
-            value={formData.email}
-            onChange={handleChange}
-            required 
-          />
-          <Input 
-            type="password"
-            size="lg"
-            label="Password"
-            crossOrigin={""}
-            typeof="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="password"
-            size="lg"
-            label="Confirm Password"
-            crossOrigin={""}
-            typeof="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <Button
-          className="mt-6"
-          fullWidth
-          type="submit"
-        >
+      <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
           Register
-        </Button>
-        <Typography color="gray" className="mt-4 text-center font-normal">
-          Already have an account?{" "}
-          <a href="/login" className="font-medium text-gray-900">
-            Login
-          </a>
         </Typography>
-      </form>
+        <Typography color="gray" className="mt-1 font-normal">
+          Please sign up below.
+        </Typography>
+        <form 
+          className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+          onSubmit={handleSubmit}
+        >
+          <div className="mb-4 flex flex-col gap-6">
+            <Input 
+              size="lg"
+              label="Email"
+              crossOrigin={""}
+              name="email"
+              type="text"
+              value={formData.email}
+              onChange={handleChange}
+              required 
+            />
+            <Input 
+              type="password"
+              size="lg"
+              label="Password"
+              crossOrigin={""}
+              typeof="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <Input
+              type="password"
+              size="lg"
+              label="Confirm Password"
+              crossOrigin={""}
+              typeof="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <Button
+            className="mt-6"
+            fullWidth
+            type="submit"
+          >
+            Register
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Already have an account?{" "}
+            <a href="/login" className="font-medium text-gray-900">
+              Login
+            </a>
+          </Typography>
+        </form>
     </Card>
       <ToastContainer position="top-center" autoClose={5000} />
         </div>
-        
-    
      );
 }
  
--- frontend/src/utils/messageFormatter.ts
+++
@@ -1,7 +0,0 @@
-/*
-    This file is used to format messages to be shown on the frontend.
-    It is used to avoid typos and to make it easier to change the messages.
-*/
-export const fm = (message: string) => {
-    return message;
-}
