448928534143f6742a90a32e59fe27be1dc62718
Chase Rumfelt <chaserumfelt@gmail.com>
2023-10-18 18:11:38 -0500

    6 month view  now it just needs one navbar to adjust each month at the same time

--- frontend/src/pages/CalendarPage.tsx
+++ frontend/src/pages/CalendarPage.tsx
@@ -8,18 +8,44 @@ type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const CalendarPage = () => {
-  const [value, onChange] = useState<Value>(new Date());
+  // Get the current date
+  const currentDate = new Date();
 
-  return(
+  // Create an array to store dates for the next 6 months, including the current month
+  const monthsArray = Array.from({ length: 6 }, (_, index) => {
+    const nextMonthDate = new Date(currentDate);
+    nextMonthDate.setMonth(currentDate.getMonth() + index);
+    return nextMonthDate;
+  });
+
+  return (
     <>
       <div className="flex flex-row">
         <HomePageSidebar />
-        <div className="flex flex-grow">
-          <Calendar onChange={onChange} value={value} />
+
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            gridTemplateRows: 'repeat(2, 1fr)',
+            gap: '10px',
+            width: '100%',
+            maxWidth: '600px',
+          }}
+        >
+          {monthsArray.map((month, index) => (
+            <div key={index}>
+              <Calendar
+                value={month}
+                showDoubleView={false}
+                showFixedNumberOfWeeks={true}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default CalendarPage;
