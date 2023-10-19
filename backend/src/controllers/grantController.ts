640509b093f4da8221c3f2709190dc4580011e36
DaronAdams <daron.cole@icloud.com>
2023-10-19 14:35:49 -0500

    Feat: Create Grant API

--- backend/src/controllers/grantController.ts
+++ backend/src/controllers/grantController.ts
@@ -1,8 +1,36 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
 export async function createGrant(req: Request, res: Response) {
-    // TODO: Implement this function
+    try {
+    const { fund, organization, account, userId, Status, startDate, endDate,
+    grantIndex } = req.body;
+
+    // Create a new grant in the database using Prisma
+    const newGrant = await prisma.grant.create({
+      data: {
+        fund,
+        organization,
+        account,
+        userId,
+        startDate,
+        endDate,
+        Status,
+        grantIndex,
+      },
+    });
+
+    // Close the Prisma connection when done
+    await prisma.$disconnect();
+
+    return res.status(201).json({ message: 'Grant created successfully', grant: newGrant });
+  } catch (error) {
+    console.error('Error creating grant:', error);
+    return res.status(500).json({ error: 'An error occurred while creating the grant' });
+  }
+
 }
 
 export async function editGrant(req: Request, res: Response) {
