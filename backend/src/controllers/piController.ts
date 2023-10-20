5446723e93993f4ec4ab5a756947bb9ae57f4afa
DaronAdams <daron.cole@icloud.com>
2023-10-20 11:13:35 -0500

    Feat: Create for grants, employees, and PIs done

--- backend/src/controllers/piController.ts
+++ backend/src/controllers/piController.ts
@@ -4,7 +4,37 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function createPi(req: Request, res: Response) {
-    // TODO: Implement this function
+    try {
+    const { uNumber, firstName, lastName, summerEffort, months, costShareEffort,
+    academicYearEffort, credit, grantId } = req.body;
+
+    const grant = await prisma.grant.findUnique({
+      where: { id: grantId },
+    });
+
+    if (!grant) {
+      return res.status(404).json({ error: 'Grant not found' });
+    }
+
+    const newPi = await prisma.pI.create({
+        data: {
+            uNumber,
+            firstName,
+            lastName,
+            summerEffort,
+            months,
+            costShareEffort,
+            academicYearEffort,
+            credit,
+            Grant: { connect: { id: grantId } },
+        },
+        });
+        await prisma.$disconnect();
+        return res.status(201).json({ message: 'PI created successfully', pi: newPi });
+    } catch (error) {
+        console.error('Error creating PI:', error);
+        return res.status(500).json({ error: 'An error occurred while creating the PI' });
+    }
 }
 
 export async function getPiById(req: Request, res: Response) {
@@ -14,4 +44,3 @@ export async function getPiById(req: Request, res: Response) {
 export async function getPiList(req: Request, res: Response) {
     // TODO: Implement this function
 }
-
