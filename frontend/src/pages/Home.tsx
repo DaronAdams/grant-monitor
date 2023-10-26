352ea1222c07af7c2b089f1cfad7664b31ec6a98
Alex Iliev <ailiev1616@gmail.com>
2023-10-25 21:26:32 -0500

    Fixed Home Page Inconsistency

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,30 +1,5 @@
-<<<<<<< HEAD
-import { HomePageSidebar } from "../components/Sidebar";
-import BasicGrid from "../components/BasicGrid";
-import UpdatedGrid from "../components/UpdatedGrid";
-
-
-
-
-const HomePage = () => {
-    return ( 
-        <>
-            <div className="flex flex-row">
-                <HomePageSidebar />
-                <div className="flex flex-col w-full">
-                    <div className="flex flex-row justify-between items-center p-4">
-                        
-                        
-                        <UpdatedGrid />
-                        
-                    </div>
-
-                </div>
-            </div>
-        </>
-=======
 import { HomePageSidebar } from '../components/Sidebar';
-import BasicGrid from '../components/BasicGrid';
+import UpdatedGrid from '../components/UpdatedGrid';
 import { useAuth } from '../hooks/context/authContext';
 
 const HomePage = () => {
@@ -37,13 +12,11 @@ const HomePage = () => {
         <div className="flex flex-col w-full">
           { user ? (<p>Welcome {user.email} </p>) : (<p>Welcome Guest</p>)}
           <div className="flex flex-row justify-between items-center p-4">
-            <BasicGrid />
+            <UpdatedGrid />
           </div>
         </div>
       </div>
     </>
->>>>>>> 41c695baba36b63b83fbbc3e5a89b7b67d12db8f
-        
 
   );
 }
