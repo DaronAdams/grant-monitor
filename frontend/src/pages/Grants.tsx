a70c058b72ad9330102bf3578a28832046b5a9f7
DaronAdams <daron.cole@icloud.com>
2023-11-11 13:39:07 -0600

    Prototyping create functions

--- frontend/src/pages/Grants.tsx
+++ frontend/src/pages/Grants.tsx
@@ -1,19 +1,22 @@
 import GrantGrid from '../components/grant/GrantGrid';
 import Subpage from '../components/Subpage';
 import NavSpeedDial from '../components/NavSpeedDial';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import GrantContainer from '../components/grant/GrantContainer';
 import GrantData from '../interfaces/GrantData';
 import { Spinner, Typography } from '@material-tailwind/react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import useGrantListData from '../hooks/grants/useGrantListData';
 import { currentGrantDataState, grantDataListState } from '../state/grants/atom';
+import { Button } from '@mui/material';
+import CreateForm from '../components/forms/CreateForm';
 
 
 const Grants = () => {
   const { isLoading } = useGrantListData();
   const grantListData: GrantData[] = useRecoilValue(grantDataListState);
   const [grantData, setGrantData] = useRecoilState(currentGrantDataState);
+  const [handleOpen, setHandleOpen] = useState(false);
   
   useEffect(() => {
     console.log('Grant List Data', grantListData);
@@ -28,6 +31,15 @@ const Grants = () => {
     setGrantData(null);
   }
 
+  const openCreateForm = () => {
+    console.log('Button clicked.. Start form procedure')
+    return true;
+  }
+
+  const closeCreateForm = () => {
+    setHandleOpen(false);
+  } 
+
   return ( 
     <div className="flex flex-row">
       <NavSpeedDial />
@@ -41,6 +53,14 @@ const Grants = () => {
             <Subpage>
               <Typography variant="h3" color="blue" style={{ padding: '10px' }}>University of Memphis Grants</Typography>
               <Typography variant="paragraph" style={{ padding: '10px' }}>Welcome, Corinne!</Typography>
+              <div className='px-4'>
+                <Button
+                  onClick={openCreateForm}
+                  variant='contained'
+                >
+                  Create Grant
+                </Button>
+              </div>
               <div className="flex flex-row justify-between items-center p-4">
                 <GrantGrid openSubpage={openSubpage} allGrantsData={grantListData} />
               </div>
