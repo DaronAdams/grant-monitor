7cfd18ec326bb165fcf5e215dd3c58e4e7efa190
kenschnarrs <knschnarrs@gmail.com>
2023-11-12 18:15:15 -0600

    added pie charts and cards, formatted and styled everything

--- frontend/src/components/grant/tabs/Main.tsx
+++ frontend/src/components/grant/tabs/Main.tsx
@@ -1,46 +1,194 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
 import GrantData from '../../../interfaces/GrantData';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import Divider from '@mui/material/Divider';
+import getDaysPassedAndRemaining from '../../../utils/getDaysPassedAndRemaining';
 
 interface GrantMainTabProps {
   grantData: GrantData;
 }
 
+interface DateDataProps {
+  daysPassed: number,
+  daysRemaining: number
+}
+
+interface GrantInfoProps {
+  description: string,
+  value: string | number,
+  hasDivider?: boolean,
+}
+
+interface CenteredContainerProps {
+  children: React.ReactNode;
+}
+
+const CenteredContainer: React.FC<CenteredContainerProps> = ({ children }) => (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    {children}
+  </div>
+);
+
+
+const CardContainer: React.FC<{ title: string, children: any }> = ({ title, children }) => (
+  <Card style={{ flex: '1', minWidth: 0, margin: '10px', overflow: 'hidden' }}>
+    <CardContent style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Typography variant="h5" component="div">
+        {title}
+      </Typography>
+      <Divider sx={{marginBottom: 1}}/>
+      <div style={{ flex: '1', overflow: 'auto' }}>{children}</div>
+    </CardContent>
+  </Card>
+);
+
+
+function formatDate(date: Date | null | undefined): string {
+
+  if (date == null || date == undefined) {
+    return 'N/A';
+  }
+
+  const thisDate = new Date(date); // the fact that I have to do this is idiotic
+
+  console.log(typeof(thisDate));
+
+  return thisDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+}
+
+const GrantInfo: React.FC<GrantInfoProps> = (grantInfo) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Set loaded to true after a short delay (you can adjust the duration)
+    const delay = setTimeout(() => {
+      setLoaded(true);
+    }, 150);
+
+    return () => clearTimeout(delay); // Cleanup timeout on unmount
+  }, []);
+
+  const textStyle: React.CSSProperties = {
+    color: grantInfo.value === 'N/A' || grantInfo.value === null ? 'lightgray' : 'inherit',
+    transition: 'opacity 0.5s ease-in-out', // CSS transition for opacity
+    opacity: loaded ? 1 : 0, // Start with opacity 0 and animate to 1 when loaded
+  };
+
+  return (
+    <Typography variant="body1">
+      <strong>{grantInfo.description}:</strong>
+      <span style={textStyle}>{' ' + (grantInfo.value || 'N/A')}</span>
+      {grantInfo.hasDivider !== false && <Divider sx={{ backgroundColor: '#ffffff' }} />}
+    </Typography>
+  );
+};
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
 const Main:React.FC<GrantMainTabProps> = ({grantData}) => {
+
+  const dateDataHolder: DateDataProps = getDaysPassedAndRemaining(grantData.startDate, grantData.endDate)
+
+  const lifetimeData = {
+    labels: ['Days Passed', 'Days Left'],
+    datasets: [
+      {
+        label: '# of Days',
+        data: [dateDataHolder.daysPassed, dateDataHolder.daysRemaining],
+        backgroundColor: [
+          'rgb(170, 18, 18, 0.7)',
+          'lightgray',
+        ],
+      },
+    ],
+  };
+
+  const spendingData = {
+    labels: ['Money Spent', 'Money Left'],
+    datasets: [
+      {
+        label: '$USD',
+        data: [15000, 36000],
+        backgroundColor: [
+          'rgb(18, 170, 18, 0.7)',
+          'lightgray',
+        ],
+      },
+    ],
+  };
 
   return(
     <div style={{
       width:'100%',
       height:'100%',
       padding:'10px',
-      border:'1px solid black',
+      border:'1px solid gray',
       borderRadius:'10px',
       margin:'10px',
     }}>
-      <h1 className="font-semibold">Grant Name</h1>
-  
-      <div>
-        <p>{`Account: ${grantData.account}`}</p>
-        <p>{`Cayuse: ${grantData.cayuse}`}</p>
-        <p>{`Fund: ${grantData.fund}`}</p>
-        <p>{`Organization: ${grantData.organization}`}</p>
-        <p>{`Program: ${grantData.program}`}</p>
-        <p>{`Sponsor: ${grantData.sponsor}`}</p>
-        <p>{`Index: ${grantData.index}`}</p>
-        <p>{`Cost-Share Index: ${grantData.costShareIndex}`}</p>
-        <p>{`NCE App Date: ${grantData.nceAppDate}`}</p>
-        <p>{`Start Date: ${grantData.startDate}`}</p>
-        <p>{`End Date: ${grantData.endDate}`}</p>
-        <p>{`Status: ${grantData.status}`}</p>
-        <p>{`Total Amount: $${grantData.totalAmount}`}</p>
-        <p>{`Yearly Amount: ${grantData.yearlyAmount}`}</p>
-        <p>{`Notes: ${grantData.notes || 'N/A'}`}</p>
-        <p>{`Created At: ${grantData.createdAt}`}</p>
-        <p>{`Updated At: ${grantData.updatedAt}`}</p>
-      </div>
+      <Typography variant="h4" component="div" sx={{ paddingBottom: 1, paddingTop: 1, paddingLeft: 1, border:'1px solid lightgray', borderRadius:'10px' }}>
+        Grant Index: {grantData.index}
+      </Typography>
 
+      <CenteredContainer>
+        <CardContainer title="Complete Info">
+          <GrantInfo description={'Account'} value={grantData.account } />
+          <GrantInfo description='Cayuse' value={grantData.cayuse} />
+          <GrantInfo description='Fund' value={grantData.fund} />
+          <GrantInfo description='Organization' value={grantData.organization} />
+          <GrantInfo description='Program' value={grantData.program} />
+          <GrantInfo description='Sponsor' value={grantData.sponsor} />
+          <GrantInfo description='Cost-Share Index' value={grantData.costShareIndex} />
+          <GrantInfo description='NCE App Date' value={formatDate(grantData.nceAppDate)} />
+          <GrantInfo description='Start Date' value={formatDate(grantData.startDate)} />
+          <GrantInfo description='End Date' value={formatDate(grantData.endDate)} />
+          <GrantInfo description='Status' value={grantData.status} />
+          <GrantInfo description='Total Amount' value={`$${grantData.totalAmount}`} />
+          <GrantInfo description='Yearly Amount' value={`$${grantData.yearlyAmount}`} />
+          <GrantInfo description='Created At' value={formatDate(grantData.createdAt)} />
+          <GrantInfo description='Updated At' value={formatDate(grantData.updatedAt)} />
+        </CardContainer>
+
+        <CardContainer title="Lifetime">
+          <Pie data={lifetimeData} />
+        </CardContainer>
+
+        <CardContainer title="Spending">
+          <Pie data={spendingData} />
+        </CardContainer>
+      </CenteredContainer>
+
+      <CenteredContainer>
+        <CardContainer title="PI's and Co-PI's">
+          <Typography variant="body2" component="div">
+            {'N/A'}
+          </Typography>
+        </CardContainer>
+      </CenteredContainer>
+
+      <CenteredContainer>
+        <CardContainer title="Employees">
+          <Typography variant="body2" component="div">
+            {'N/A'}
+          </Typography>
+        </CardContainer>
+      </CenteredContainer>
+
+      <CenteredContainer>
+        <CardContainer title="Notes">
+          <Typography variant="body2" component="div">
+            {grantData.notes || 'No notes for this grant'}
+          </Typography>
+        </CardContainer>
+      </CenteredContainer>
     </div>
-      
-
   ) 
 }
 
